292172ff332a77d22c8b7a1581bc62838d3db2ce
gaalih <galih@mail.unpas.ac.id>
2022-04-27 20:15:19 +0700

    feat: add categories and search add routes to gel all categories, get data from specific category and search program

--- modules/program.js
+++ modules/program.js
@@ -4,7 +4,7 @@ var data_program = require('../datas/program.json')
 
 // middleware that is specific to this router
 router.use((req, res, next) => {
-  console.log('Time: ', Date.now())
+  console.log('Access Date: ', Date.now())
   next()
 })
 
@@ -13,17 +13,63 @@ router.get('/', (req, res) => {
   res.status(200).send(data_program)
 })
 
-// specific data
+// all categories
+router.get('/categories', (req, res) => {
+  var results = []
+  for (var i = 0; i < data_program.length; i++) {
+    results.push(data_program[i]['kategori'])
+  }
+  results = [...new Set(results)]
+  res.status(200).send(results)
+})
+
+// search from specific category
+router.get('/category/:category_name', (req, res) => {
+  const { category_name } = req.params
+  var results = []
+  for (var i = 0; i < data_program.length; i++) {
+    if (data_program[i]['kategori'] == category_name) {
+      results.push(data_program[i])
+    }
+  }
+
+  if (results.length === 0 || category_name == null) {
+    res.status(418).send({
+      message: `Data Dengan Kategori ${category_name} Tidak Ditemukan!`,
+    })
+  }
+  res.status(200).send(results)
+})
+
+// specific data from id
 router.get('/:id_program', (req, res) => {
-  let program_dicari = data_program.find(
+  let program_dipilih = data_program.find(
     (x) => x.id === parseInt(req.params.id_program),
   )
-
   // jika data tidak ditemukan
-  if (program_dicari == null) {
+  if (program_dipilih == null) {
     res.status(418).send({ message: 'Data Program Tidak Ditemukan!' })
   }
-  res.send(program_dicari)
+  res.status(200).send(program_dipilih)
+})
+
+// search some data from judul
+router.get('/search/judul=:cari_nama_program', (req, res) => {
+  const { cari_nama_program } = req.params
+  var results = []
+  for (var i = 0; i < data_program.length; i++) {
+    var judul = data_program[i]['judul']
+    if (judul.includes(`${cari_nama_program}`)) {
+      results.push(data_program[i])
+    }
+  }
+  // jika data tidak ditemukan
+  if (results.length === 0 || cari_nama_program == null) {
+    res
+      .status(418)
+      .send({ message: `Data Program ${cari_nama_program} Tidak Ditemukan!` })
+  }
+  res.status(200).send(results)
 })
 
 module.exports = router
